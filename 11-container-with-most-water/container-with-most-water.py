8c4cbfbd7ddae3e9cb82a065e4bd56eeb98816b1
Pradeep Kumar Maurya <62380961+pradeep-kumar-maurya@users.noreply.github.com>
2023-09-05 20:27:43 +0530

    Time: 554 ms (96.07%) | Memory: 29.3 MB (43.37%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,8 +1,17 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
+        """
+        The logic is to use two pointers and keep calculating the water contained between two lines.
+        If height[i] < height[j], then first calculate the contained water using the smaller value among two and multiply
+        it with their indexes difference. Then increment i with 1 because height[i] was smaller and moving to right could
+        yield in greater contained water.
+        If height[i] >= height[j], then first calculate the contained water using the smaller value among two and multiply
+        it with their indexes difference. Then decrement j with 1 because height[j] was less than or smaller and moving
+        to left could yield in greater contained water.
+        """
         i, j = 0, len(height)-1
         max_water = 0
-        while i != j:
+        while i != j:  # breaking condition is when both i & j are pointing to the same index
             if height[i] < height[j]:
                 container_water = height[i] * (j - i)
                 if container_water > max_water:
@@ -14,4 +23,3 @@ class Solution:
                     max_water = container_water
                 j -= 1
         return max_water
-        
