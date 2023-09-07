c9df3623feaf950dcb292778c6e9102d3d8d8af5
Pradeep Kumar Maurya <62380961+pradeep-kumar-maurya@users.noreply.github.com>
2023-09-07 16:32:38 +0530

    Time: 75 ms (40.70%) | Memory: 16.5 MB (44.23%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -4,22 +4,30 @@ class Solution:
         count = 0
         freq_dict = {}
 
-        for i, char in enumerate(s):  # bpfbhmipx
+        # Solving using a hashmap
+        for i, char in enumerate(s):
             data = freq_dict.get(char)
-            if data is None:
+            if data is None:  # if the char is absent in hashmap then increment count by 1
                 freq_dict[char] = i
                 count += 1
                 if count > max_count:
                     max_count = count
             else:
-                main_index = data
-                index = data
+                main_index = data  # fetch the index and store in one variable
+                index = data  # again save the same index in another variable
+                """
+                The idea is to remove the duplicate element from the hashmap and also any other element that appears
+                before the duplicate element in the string 's', therefore, a while loop is used with 3 conditions i.e.
+                index >= 0 because python follows -ve indexing too.
+                freq_dict.get(s[index]) is not None because if the data is None then we don't need to iterate further.
+                freq_dict.get(s[index]) <= main_index otherwise it can remove a valid substring element which we don't want.
+                """
                 while index >= 0 and freq_dict.get(s[index]) is not None and freq_dict.get(s[index]) <= main_index:
-                    freq_dict.pop(s[index])
-                    index -= 1
-                    count -= 1
-                freq_dict[char] = i
-                count += 1
+                    freq_dict.pop(s[index])  # pop the element
+                    index -= 1  # decrease index by 1
+                    count -= 1  # decrease count by 1
+                freq_dict[char] = i  # after the while loop ends add char to hashmap with new index value i.e. 'i'
+                count += 1  # increase count by 1
                 if count > max_count:
                     max_count = count
 
