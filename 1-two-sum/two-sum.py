28d36f9cc52ee758b14745213151029681187bda
Pradeep Kumar Maurya <62380961+pradeep-kumar-maurya@users.noreply.github.com>
2023-09-08 21:36:14 +0530

    Time: 69 ms (63.83%) | Memory: 17.5 MB (50.70%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,17 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         freq_dict = {}
-        for i, value in enumerate(nums):
-            if freq_dict.get(value) is None:
-                freq_dict[value] = [i]
-            else:
-                freq_dict[value].append(i)
-        for i, value in enumerate(nums):
-            element = target - value
-            data = freq_dict.get(element)
-            if data is not None and len(data) == 1 and data[0] > i:
-                return [i, data[0]]
-            elif data is not None and len(data) > 1:
-                for j in data:
-                    if j > i:
-                        return [i, j]
+        for i in range(len(nums)):
+            freq_dict[nums[i]] = i
+        for i in range(len(nums)):
+            j = freq_dict.get(target - nums[i])
+            if j and j > i:
+                return [i, j]
