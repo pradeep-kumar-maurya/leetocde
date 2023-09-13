d5d5ada1a9b3e460f01618dfb4af7e501a8babf7
Pradeep Kumar Maurya <62380961+pradeep-kumar-maurya@users.noreply.github.com>
2023-09-13 15:04:40 +0530

    Time: 110 ms (37.81%) | Memory: 21.8 MB (30.60%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,5 +1,28 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        """
+        The idea is to use a freq_array of size 26 because the string contains only lower case alphabets.
+        After creating the freq_array per string, we need to check in the freq_dict as a tuple(freq_arr).
+        Overall T.C = O(N*M)
+        """
+        freq_dict = {}
+        for string in strs:  # O(N)
+            freq_arr = [0] * 26  # a .... z -> 0 ... 25
+            for char in string:  # O(M) where M is the length of the string. Max M could be 100 in this case.
+                freq_arr[ord(char) - ord("a")] += 1
+            if freq_dict.get(tuple(freq_arr)) is None:
+                freq_dict[tuple(freq_arr)] = [string]
+            else:
+                freq_dict[tuple(freq_arr)].append(string)
+        return freq_dict.values()
+
+    """
+    This approach uses sorting the string
+    def solve(strs):
+        '''
+        The idea is just to sort the strings in strs array and check in the freq dict.
+        I guess overall T.C = O(N^2 logN)
+        '''
         freq_dict = {}
         for string in strs:  # O(N)
             sorted_str = "".join(sorted(string))  # O(NlogN)
@@ -8,4 +31,4 @@ class Solution:
             else:
                 freq_dict[sorted_str].append(string)
         return freq_dict.values()
-
+    """
