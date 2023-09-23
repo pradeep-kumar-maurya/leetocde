817a922baba304ec66e183bd447c283fda40ee6b
Pradeep Kumar Maurya <62380961+pradeep-kumar-maurya@users.noreply.github.com>
2023-09-23 18:39:48 +0530

    Time: 189 ms (78.46%) | Memory: 23.9 MB (69.06%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,23 +1,13 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        prefix_mul = [0] * len(nums)
-        prefix_mul[0] = nums[0]
-        suffix_mul = [0] * len(nums)
-        suffix_mul[-1] = nums[-1]
-        ans_array = []
-
-        for i in range(1, len(nums), 1):
-            prefix_mul[i] = prefix_mul[i-1] * nums[i]
-
-        for i in range(len(nums)-2, -1, -1):
-            suffix_mul[i] = suffix_mul[i+1] * nums[i]
-
-        for i in range(len(nums)):
-            if i == 0:
-                ans_array.append(suffix_mul[1])
-            elif i == len(nums)-1:
-                ans_array.append(prefix_mul[len(nums)-2])
-            else:
-                ans_array.append(prefix_mul[i-1] * suffix_mul[i+1])
-
-        return ans_array
+        ans = [0] * len(nums)
+        ans[0] = 1
+        temp = 1
+        for i in range(0, len(nums)-1, 1):
+            temp = temp * nums[i]
+            ans[i+1] = temp
+        temp = 1
+        for i in range(len(nums)-1, 0, -1):
+            temp = temp * nums[i]
+            ans[i-1] = ans[i-1] * temp
+        return ans
