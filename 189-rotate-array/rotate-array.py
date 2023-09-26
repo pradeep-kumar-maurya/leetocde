1f78f1c846cd2b2011c9ed4465778fb6f01f2583
Pradeep Kumar Maurya <62380961+pradeep-kumar-maurya@users.noreply.github.com>
2023-09-27 00:04:24 +0530

    Time: 191 ms (38.39%) | Memory: 27.8 MB (61.23%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -4,7 +4,7 @@ class Solution:
     Then reverse the subarray from index 0 -> k-1
     Then reverse the subarray from index k -> len(nums)-1
     """
-    def swap(self, nums, i, j):
+    def swap(self, nums, i, j):  # this will swap the array
         while i < j:
             temp = nums[j]
             nums[j] = nums[i]
@@ -13,9 +13,9 @@ class Solution:
             j -= 1
 
     def rotate(self, nums, k):
-        k = k % len(nums)  # for larger values of k, modulo (%) will bring down the k value
+        k = k % len(nums)  # for larger values of k, modulo (%) will bring down the k value in range [0, len(nums)-1]
         if k == 0:
             return
-        self.swap(nums, 0, len(nums)-1)
-        self.swap(nums, 0, k-1)
-        self.swap(nums, k, len(nums)-1)
+        self.swap(nums, 0, len(nums)-1)  # this will swap the entire array
+        self.swap(nums, 0, k-1)  # this will swap the subarray from index 0 -> k-1
+        self.swap(nums, k, len(nums)-1)  # this will swap the subarray from index k -> len(nums)-1
