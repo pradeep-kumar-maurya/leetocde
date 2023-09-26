e630d8e99be94552f7211271c0563ee244a82609
Pradeep Kumar Maurya <62380961+pradeep-kumar-maurya@users.noreply.github.com>
2023-09-27 00:22:47 +0530

    Time: 37 ms (82.37%) | Memory: 16.1 MB (99.55%) - LeetSync

--- 389-find-the-difference/find-the-difference.py
+++ 389-find-the-difference/find-the-difference.py
@@ -1,5 +1,6 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
+        # The idea is to create two dictionaries and then compare the keys/values of the two dicts.
         dict_s = {}
         dict_t = {}
 
@@ -16,7 +17,7 @@ class Solution:
                 dict_t[char] += 1
 
         for key in dict_t.keys():
-            if dict_s.get(key) is None:
+            if dict_s.get(key) is None:  # If the key is not found in dict_s then key is the extra char
                 return key
-            elif dict_s.get(key) != dict_t.get(key):
+            elif dict_s.get(key) != dict_t.get(key):  # If the value of the key does not match then key is the extra char
                 return key
