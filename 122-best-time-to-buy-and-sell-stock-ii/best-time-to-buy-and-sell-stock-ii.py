fce17696003c8e91d48d2667b2b768d7c9e71122
Pradeep Kumar Maurya <62380961+pradeep-kumar-maurya@users.noreply.github.com>
2023-09-28 01:47:28 +0530

    Time: 70 ms (25.13%) | Memory: 17.8 MB (52.05%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,5 +1,9 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        """
+        The idea is that, the max profit would be achieved if prices[i] > prices[i-1].
+        Therefore, we just sum the differences whenever prices[i] > prices[i-1].
+        """
         max_profit = 0
         for i in range(1, len(prices), 1):
             if prices[i] > prices[i - 1]:
