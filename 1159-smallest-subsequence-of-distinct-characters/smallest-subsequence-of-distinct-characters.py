20c90d3be6a7e88cbbb588de8540b3f92a6b399d
Pradeep Kumar Maurya <62380961+pradeep-kumar-maurya@users.noreply.github.com>
2023-09-28 23:14:44 +0530

    Time: 42 ms (45.51%) | Memory: 16.1 MB (81.68%) - LeetSync

---
+++ 1159-smallest-subsequence-of-distinct-characters/smallest-subsequence-of-distinct-characters.py
@@ -0,0 +1,35 @@
+class Solution:
+    def smallestSubsequence(self, s: str) -> str:
+        freq_dict = {}
+        boolean_array = [False] * 26
+        ans = []
+
+        for char in s:
+            if freq_dict.get(char) is None:
+                freq_dict[char] = 1
+            else:
+                freq_dict[char] += 1
+
+        ans.append(s[0])
+        freq_dict[s[0]] -= 1
+        boolean_array[ord(s[0]) - ord('a')] = True
+
+        for i in range(1, len(s), 1):
+            if boolean_array[ord(s[i]) - ord('a')] is False:
+                for j in range(len(ans)-1, -1, -1):
+                    if freq_dict.get(ans[j]) == 0 or s[i] > ans[j]:
+                        ans.append(s[i])
+                        boolean_array[ord(s[i]) - ord('a')] = True
+                        freq_dict[s[i]] -= 1
+                        break
+                    else:
+                        boolean_array[ord(ans[j]) - ord('a')] = False
+                        ans.pop(j)
+                else:
+                    ans.append(s[i])
+                    boolean_array[ord(s[i]) - ord('a')] = True
+                    freq_dict[s[i]] -= 1
+            else:
+                freq_dict[s[i]] -= 1
+
+        return ''.join(ans)
