ca5ae0a215ce5ef1c1753e4d0ca2bab16e15059e
Pradeep Kumar Maurya <62380961+pradeep-kumar-maurya@users.noreply.github.com>
2023-10-03 23:37:46 +0530

    Time: 120 ms (37.62%) | Memory: 17.2 MB (44.23%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -1,17 +1,23 @@
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        The idea is to iterate over the matrix and find the rows and columns where matrix[row][col] == 0.
+        We will add rows to a set and also add columns to a set wherever row and column is pointing to 0.
+        """
         rows_with_zero = set()
         cols_with_zero = set()
 
+        # iterate over the matrix and find rows and columns that point to 0
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
                 if matrix[i][j] == 0:
-                    rows_with_zero.add(i)
-                    cols_with_zero.add(j)
+                    rows_with_zero.add(i)  # add rows to rows_with_zero set
+                    cols_with_zero.add(j)  # add columns to cols_with_zero
 
+        # iterate over the matrix and manipulate the cells to 0 by checking rows and columns in the set
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
-                if i in rows_with_zero:
+                if i in rows_with_zero:  # if row is in rows_with_zero then set the cell to 0
                     matrix[i][j] = 0
-                if j in cols_with_zero:
+                if j in cols_with_zero:  # if column is in cols_with_zero then set the cell to 0
                     matrix[i][j] = 0
