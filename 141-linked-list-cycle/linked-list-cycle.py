9656875296dc0db213dc7f0d7dc44265f9eca05d
Pradeep Kumar Maurya <62380961+pradeep-kumar-maurya@users.noreply.github.com>
2023-10-09 19:08:23 +0530

    Time: 65 ms (17.46%) | Memory: 20.6 MB (21.43%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.py
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -6,16 +6,22 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        id_set = set()
+        """
+        The idea is to store all the memroy id of the nodes in a set and if there is any cycle then
+        the memory id of a particular node will definitely be in the set. If it's in set return True
+        else retrun False after the while loop completes.
+        """
+        id_set = set()  # set to store node memory id
 
+        # Iterate till the tail
         while head is not None:
 
-            if id(head) in id_set:
+            if id(head) in id_set:  # if node id found in the set then return True
                 return True
 
-            else:
+            else:  # else add that node id in the set
                 id_set.add(id(head))
 
-            head = head.next
+            head = head.next  # move to next node
 
         return False
