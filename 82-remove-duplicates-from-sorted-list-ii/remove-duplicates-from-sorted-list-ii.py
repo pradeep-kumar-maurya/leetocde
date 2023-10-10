f9e4796b6a50a223af7edf245f8429c893471ab1
Pradeep Kumar Maurya <62380961+pradeep-kumar-maurya@users.noreply.github.com>
2023-10-11 00:33:16 +0530

    Time: 46 ms (55.54%) | Memory: 16.4 MB (37.94%) - LeetSync

--- 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.py
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.py
@@ -5,38 +5,49 @@
 #         self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        uniqueElements = set()
-        tempSet = set()
+        """
+        The idea is to first prepare a set that only has unique elements in it. Now iterate over the linked list and
+        only consider the nodes that have their values in the unique element set. Smartly link the valid nodes and remove
+        the duplicate nodes.
+        """
+        uniqueElements = set()  # set which will contain unique elements
+        tempSet = set()  # set which will help in creating the uniqueElements set
         tempHead = head
 
+        # Iterate over the linked list and create the uniqueElements set
         while tempHead:
             val = tempHead.val
+            # condition to check the val is unique
             if val not in tempSet and val not in uniqueElements:
                 uniqueElements.add(val)
+            # if val in uniqueElements and not in tempSet then remove from uniqueElements and add to tempSet
             elif val not in tempSet and val in uniqueElements:
                 tempSet.add(val)
                 uniqueElements.remove(val)
+            # if val already in tempSet then it's a duplicate and don't do anything
             elif val in tempSet:
                 pass
             tempHead = tempHead.next
 
         tempHead = head
-        newHead = None
-        second_node = None
+        newHead = None  # it will contain the head of the unique linked list
+        secondNode = None  # secondNode will be used to link the unique nodes
 
         while tempHead:
+            # if the node is in uniqueElements then consider the node
             if tempHead.val in uniqueElements:
-                if newHead is None:
-                    second_node = tempHead
-                    newHead = second_node
+                if newHead is None:  # if it's the first node then set newHead to node
+                    secondNode = tempHead
+                    newHead = secondNode
                     tempHead = tempHead.next
-                else:
-                    second_node.next = tempHead
-                    second_node = second_node.next
+                else:  # else keep on linking nodes to secondNode and keep incrementing secondNode
+                    secondNode.next = tempHead
+                    secondNode = secondNode.next
                     tempHead = tempHead.next
+            # else keep incrementing tempHead
             else:
                 tempHead = tempHead.next
 
-        if second_node:
-            second_node.next = None
+        if secondNode:  # if secondNode is not None then it will be the last node therefore point next to None
+            secondNode.next = None
         return newHead
