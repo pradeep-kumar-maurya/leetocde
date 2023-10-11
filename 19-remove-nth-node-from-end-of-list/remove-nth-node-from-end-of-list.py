6e81279ee9138cbd4f7f7ae2b4742778e489cad5
Pradeep Kumar Maurya <62380961+pradeep-kumar-maurya@users.noreply.github.com>
2023-10-12 01:23:05 +0530

    Time: 45 ms (23.47%) | Memory: 16.1 MB (97.26%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -5,6 +5,10 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        """
+        The idea is to find the nth node that has to be removed.
+        This node can be calculated by (length of linked list - n + 1).
+        """
         tempHead = head
         linked_list_length = 0
 
@@ -15,16 +19,18 @@ class Solution:
         nth_node_to_delete = linked_list_length - n + 1
         tempHead = head
         count = 0
+        # We will maintain a dummy node which will be the left node of the deleting node and will point to the next node of
+        # the deleting node.
         dummy_node = head
 
         while tempHead:
             count += 1
             if count == nth_node_to_delete:
-                if count == 1:
+                if count == 1:  # if count is 1 that means we need to delete the head therefore set head to head.next
                     head = head.next
-                else:
+                else:  # if count is > 1 then point dummy_node.next to tempHead.next
                     dummy_node.next = tempHead.next
-            dummy_node = tempHead
+            dummy_node = tempHead  # this dummy node should be 1 less than the tempHead node
             tempHead = tempHead.next
 
         return head
