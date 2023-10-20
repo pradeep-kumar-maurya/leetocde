bb818f28b22e0c1771e6af61d97b2074cd6a1f11
Pradeep Kumar Maurya <62380961+pradeep-kumar-maurya@users.noreply.github.com>
2023-10-21 02:15:52 +0530

    Time: 38 ms (85.43%) | Memory: 17.1 MB (86.10%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
@@ -1,12 +1,3 @@
-"""
-# Definition for a Node.
-# class Node:
-#     def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
-#         self.val = int(x)
-#         self.next = next
-#         self.random = random
-"""
-
 class Node:
     def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
         self.val = int(x)
@@ -15,13 +6,21 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        """
+        The idea is to create new nodes and link them in order as they are in the original list. Now in order to point
+        random to the newly created nodes we will be needing a hashmap where the key will be a node from the original
+        list and value will be the same node but newly created one.
+        """
         tempHead = head
-        deepcopyNodeDict = {}
-        newHead = None
+        deepcopyNodeDict = {}  # hashmap to store old and new nodes
+        newHead = None  # this will store the head
         tempNode = None
 
+        # below while loop will link the newly created nodes
         while tempHead:
+            # we will create new nodes using the Node class
             node = Node(x=tempHead.val, next=tempHead.next, random=tempHead.random)
+            # key will be the old node and value will be its new node
             deepcopyNodeDict[tempHead] = node
             if newHead is None:
                 newHead = node
@@ -33,6 +32,7 @@ class Solution:
 
         tempHead = newHead
 
+        # below while loop will make random to point to the newly created nodes instead of the old ones
         while tempHead:
             data = deepcopyNodeDict.get(tempHead.random)
             tempHead.random = data
