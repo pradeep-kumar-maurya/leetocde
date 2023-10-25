180ad98b8cfeb0c90d6825061f842639fa919e8b
Pradeep Kumar Maurya <62380961+pradeep-kumar-maurya@users.noreply.github.com>
2023-10-25 15:33:55 +0530

    Time: 310 ms (78.22%) | Memory: 63.6 MB (57.74%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -1,26 +1,35 @@
 class RandomizedSet:
 
     def __init__(self):
-        self.map = set()
+        self.values = []
+        self.map = {}
 
     def insert(self, val: int) -> bool:  # this is O(1)
-        if val in self.map:
+        if self.map.get(val) is not None:  # "is not None" is important because index 0 can cause "if" to fail
             return False
         else:
-            self.map.add(val)
+            self.values.append(val)
+            self.map[val] = len(self.values) - 1
             return True
 
     def remove(self, val: int) -> bool:  # this is O(1)
-        if val in self.map:
-            self.map.remove(val)
+        if self.map.get(val) is not None:  # "is not None" is important because index 0 can cause "if" to fail
+            index = self.map.get(val)
+            if index == len(self.values) - 1:
+                self.values.pop()
+                self.map.pop(val)
+            else:
+                self.values[index] = self.values[-1]
+                self.values[-1] = val
+                self.values.pop()
+                self.map.pop(val)
+                self.map[self.values[index]] = index
             return True
         else:
             return False
 
     def getRandom(self) -> int:
-        return random.choice(list(self.map))  # this is O(N) because set is being converted to a list everytime
-        
-
+        return random.choice(self.values)  # this is O(1)
 
 # Your RandomizedSet object will be instantiated and called as such:
 # obj = RandomizedSet()
