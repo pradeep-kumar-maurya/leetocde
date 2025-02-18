1d70c539945c7f0c5b81964580be2e0fb8934467
Pradeep Kumar Maurya <62380961+pradeep-kumar-maurya@users.noreply.github.com>
2025-02-19 00:12:34 +0530

    Time: 171 ms (6.84%) | Memory: 34.6 MB (15.55%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -1,5 +1,9 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        '''
+        VVVVVVVImp:- Whenever we are asked about the next greatest element to a specific element in an array,
+        we should always think of "stacks".
+        '''
         stack = []
         top = -1
         ans = [0] * len(temperatures)
@@ -10,16 +14,27 @@ class Solution:
                 stack.append([temperatures[i], i])
 
             else:
+                # If array element is < stack top element, just add the element to the stack
                 if temperatures[i] < stack[top][0]:
                     top += 1
                     stack.append([temperatures[i], i])
                 else:
+                    '''
+                    Whenever the "array element" is > "stack top element", keep removing elements from the stack.
+                    For all the removed elements from the stack, the "array element" will be its "next greatest element".
+                    '''
                     while top > -1 and temperatures[i] > stack[top][0]:
+                        '''
+                        Add all the removed data from the stack to any data structure along with the array element
+                        which was removing it. The data in the data structure gives us the info about array elements
+                        and their corresponding next greatest elements.
+                        '''
                         index = stack[top][1]
                         ans[index] = i - index
                         stack.pop(top)
                         top -= 1
 
+                    # Now add that array element into the stack
                     top += 1
                     stack.append([temperatures[i], i])
 
